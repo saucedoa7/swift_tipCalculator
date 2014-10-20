e56ad349bcd27d7a72aa3864a82c492a1df0d242
Albert Saucedo <saucedoa7@gmail.com>
2014-10-19 22:35:39 -0500

    Auto layout for all iPhone sizes. Added at total price.

--- SwiftTipCalc/ViewController.swift
+++ SwiftTipCalc/ViewController.swift
@@ -10,16 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mealCostTextField: UITextField!
+    @IBOutlet weak var tipPercentTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var mealTotalLabel: UILabel!
+
+
+    var mealcost = ""
+    var tipPercenatge = ""
+    var tip : Float = 0.0
+    var total : Float = 0.0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btnCalculate(sender: UIButton) {
+        calculateTip()
     }
 
+    @IBAction func btnClear(sender: UIButton) {
+        cleartextFields()
+    }
 
+    func cleartextFields () -> Bool{
+        mealCostTextField.text = ""
+        tipPercentTextField.text = ""
+
+        tipAmountLabel.text = "Tip"
+        mealTotalLabel.text = "Total"
+        return true
+    }
+
+    func calculateTip () -> Bool{
+        mealcost = mealCostTextField.text
+        tipPercenatge = tipPercentTextField.text
+        // convert string to float
+        var fmealCost = (mealcost as NSString).floatValue
+        var fTipPercentage = (tipPercenatge as NSString).floatValue
+
+        tip = fmealCost * (fTipPercentage * 0.01)
+        total = fmealCost + tip
+
+        var tipFormat :NSString = NSString(format: "Tip: $%.2f", tip)
+        var totalFormat :NSString = NSString(format: "Total $%.2f", total)
+
+        tipAmountLabel.text = "\(tipFormat)"
+        mealTotalLabel.text = "\(totalFormat)"
+
+        return true
+    }
 }
-
