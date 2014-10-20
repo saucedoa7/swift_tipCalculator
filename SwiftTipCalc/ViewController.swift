ac0fa92c62109a10223c9cae663987b840d8bc7f
Albert Saucedo <saucedoa7@gmail.com>
2014-10-19 20:26:11 -0500

    Initial Commit

---
+++ SwiftTipCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTipCalc
+//
+//  Created by Albert Saucedo on 10/19/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
